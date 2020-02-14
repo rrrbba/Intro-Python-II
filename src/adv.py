f1df0f7469d2d877c8b12d1d5a6aee06c83cd4f2
Rayven <rayven.burns@gmail.com>
2020-02-13 20:12:54 -0500

    mvp done

--- src/adv.py
+++ src/adv.py
@@ -3,29 +3,46 @@ from player import Player
 from item import Item
 # Declare all the rooms
 
-# items = {
-#     'potion': Item("a potion", """Heals your wounds"""),
-#     'sword': Item("a sword", """To fight the bad guys"""),
-#     'shoe': Item("an old shoe", """Looks like someone left this here"""),
-#     'reward': Item("gold", """Congratulations!"""),
-# }
+items = [
+     Item("potion", """Heals your wounds"""),
+    Item("sword", """To fight the bad guys"""),
+    Item("shoe", """Looks like someone left this here"""),
+    Item("gold", """Congratulations!"""),
+]
 
+# room = {
+#     'outside':  Room("Outside Cave Entrance", """North of you, the cave mount beckons.""",Item("No items", """---""")),
+
+#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+# passages run north and east.""", Item("a potion", """Heals your wounds""")), 
+
+#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+# into the darkness. Ahead to the north, a light flickers in
+# the distance, but there is no way across the chasm.""", Item("a sword", """To fight the bad guys""")), 
+
+#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+# to north. The smell of gold permeates the air.""", Item("an old shoe", """Looks like someone left this here""")), 
+
+#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+# chamber! Sadly, it has already been completely emptied by
+# earlier adventurers. The only exit is to the south.""", Item("gold", """Congratulations!""")), 
+# }
 room = {
-    'outside':  Room("Outside Cave Entrance", """North of you, the cave mount beckons.""",Item("No items", """---""")),
+    'outside':  Room("Outside Cave Entrance", """North of you, the cave mount beckons.""", items[0]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", Item("a potion", """Heals your wounds""")), 
+passages run north and east.""", items[0]), 
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", Item("a sword", """To fight the bad guys""")), 
+the distance, but there is no way across the chasm.""", items[1]), 
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", Item("an old shoe", """Looks like someone left this here""")), 
+to north. The smell of gold permeates the air.""", items[2]), 
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", Item("gold", """Congratulations!""")), 
+earlier adventurers. The only exit is to the south.""", items[3]), 
 }
 
 
@@ -61,7 +78,7 @@ while True:
     print("\nYour position is currently: ", player.current_room.name)
     print("\nThe description of your surroundings is: ", player.current_room.description)
    
-    choice = input("\n\nPlease enter [n] North   [s] South  [e] East  [w] West to move or  [q] Quit \n")
+    choice = input("\n\nPlease enter [n] North   [s] South  [e] East  [w] West to move   [i/inventory] Inventory or  [q] Quit \n")
 
 
     
@@ -70,8 +87,7 @@ while True:
             print("\nYou moved north!\n")
             
             player.current_room = player.current_room.n_to
-            print("This room has a: ", player.current_room.item)
-        
+            print("Item: ", player.current_room.item)
         else: 
             print("You're going the wrong way! Try again.")
     elif choice == 's':
@@ -100,12 +116,14 @@ while True:
             print("\nYou're going the wrong way! Try again.")
     elif choice == 'take':
         player.items.append(player.current_room.item.name)
-        del player.current_room.item
+        player.current_room.item = None
         print("You took an item!")
-
-    elif choice == 'my items' or choice == 'i':
+    elif choice == 'drop':
+        player.items.pop()
+        print("You dropped an item!")
+    elif choice == 'inventory' or choice == 'i':
         stuff = ""+", ".join(player.items)
-        print(f"\nYour items: {stuff}")
+        print(f"\nYour inventory: {stuff}")
     elif choice == 'q':
         print("\nThanks for playing!\n")
         break
